c7dba44676b28f477540848193ca479c73ef591b
VuCuongHD96 <xuancuonghd1102@gmail.com>
2023-07-29 03:21:09 +0700

    Update cosmos view

--- SwiftUI_Playground.swiftpm/CosmosView.swift
+++ SwiftUI_Playground.swiftpm/CosmosView.swift
@@ -13,11 +13,22 @@ struct CosmosView: View {
         static let squareEdge: CGFloat = 50
     }
     
-    @State var rating: Double = 3.3
+    @State var rating: Double = 3.0
     var starSpacing: CGFloat = 10
     
     var body: some View {
         VStack {
+            HStack {
+                Image(systemName: "plus.viewfinder")
+                    .onTapGesture {
+                        rating -= 0.1
+                    }
+                Text("rating = \(rating)")
+                Image(systemName: "minus.circle")
+                    .onTapGesture {
+                        rating += 0.1
+                    }
+            }
             Slider(value: $rating, in: 0...5, step: 0.1)
             starsView
                 .overlay(overlayView)//.mask(starsView))
@@ -48,13 +59,9 @@ struct CosmosView: View {
     func calculateWidth() -> CGFloat {
         let naturalParts = Int(rating)
         let decimalPart = rating - Double(naturalParts)
-        var numberOfSpace = 0
-        if naturalParts != 0 {
-            numberOfSpace = min(naturalParts, Int(rating))
-        }
-        
+  
         let result1: CGFloat = CGFloat(naturalParts) * Constant.squareEdge
-        let result2: CGFloat = CGFloat(numberOfSpace) * starSpacing
+        let result2: CGFloat = CGFloat(naturalParts) * starSpacing
         print("---- debug ------- result2 = ", result2)
 
         let result3: CGFloat = decimalPart * Constant.squareEdge
